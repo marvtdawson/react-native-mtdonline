2f9e45431534c0cdff8a0a5548056924f7cb162c
Marvin Dawson <marvintdawson@gmail.com>
2019-09-20 04:17:15 -0500

    added key to SeriesListItem

--- src/components/SeriesList/index.js
+++ src/components/SeriesList/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import './index.css';
+
+const SeriesListItem = ({ series }) => (
+    <li>
+        {series.show.name}
+    </li>
+);
 const SeriesList = (props) => {
     return(
         <ul className="series-list">
             {props.list.map(series => (
-                <li key={series.show.id}>
-                    {series.show.name}
-                </li>
+               <SeriesListItem series={series} key={series.show.id} />
             ))}
         </ul>
     )
